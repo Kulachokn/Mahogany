a9a4c0c3dfbfdc3e8d03b2b7b1ddee8aae3cfa9f
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2023-11-15 13:52:21 +0100

    optimized library page + added loader

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import SpotifyWebApi from "spotify-web-api-node";
+import { MutatingDots } from "react-loader-spinner";
 
 import TrackSearchResult from "../components/TrackSearchResult";
 import Player from "../components/Player";
@@ -9,56 +10,59 @@ const spotifyApi = new SpotifyWebApi({
   clientId: "62123b4608c441cb9d53b6c93a965bac",
 });
 
-const Library = ({ code }) => {
-  const accessToken = localStorage.getItem("accessToken");
-
-  spotifyApi.setAccessToken(accessToken);
+const Library = () => {
   const [savedTracks, setSavedTracks] = useState([]);
-  const [playingTrack, setPlayingTrack] = useState();
+  const [playingTrack, setPlayingTrack] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const accessToken = localStorage.getItem("accessToken");
+
+        if (!accessToken) {
+          setLoading(false);
+          return;
+        }
+
+        spotifyApi.setAccessToken(accessToken);
+
+        const response = await spotifyApi.getMySavedTracks({
+          limit: 10,
+          offset: 1,
+        });
+
+        const tracks = response.body.items.map((item) => {
+          const smallestAlbumImage = item.track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            item.track.album.images[0]
+          );
+
+          return {
+            artist: item.track.artists[0].name,
+            title: item.track.name,
+            uri: item.track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        });
+
+        setSavedTracks(tracks);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const chooseTrack = (track) => {
     setPlayingTrack(track);
   };
-
-  useEffect(() => {
-    if (!accessToken) return;
-    spotifyApi
-      .getMySavedTracks({
-        limit: 10,
-        offset: 1,
-      })
-      .then(
-        (res) =>
-          setSavedTracks(
-            res.body.items.map((item) => {
-              const smallestAlbumImage = item.track.album.images.reduce(
-                (smallest, image) => {
-                  if (image.height < smallest.height) return image;
-                  return smallest;
-                },
-                item.track.album.images[0]
-              );
-
-              return {
-                artist: item.track.artists[0].name,
-                title: item.track.name,
-                uri: item.track.uri,
-                albumUrl: smallestAlbumImage.url,
-              };
-            })
-          )
-        // function (err) {
-        //   console.log("Something went wrong!", err);
-        // }
-      );
-  }, [accessToken]);
-
-  spotifyApi.removeFromMySavedTracks(["3VNWq8rTnQG6fM1eldSpZ0"])
-  .then(function(data) {
-    console.log('Removed!');
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  });
 
   return (
     <>
@@ -68,18 +72,42 @@ const Library = ({ code }) => {
       >
         <h1>My Library</h1>
         <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-          {savedTracks.map((track) => (
-            <TrackSearchResult
-              track={track}
-              key={track.uri}
-              chooseTrack={chooseTrack}
+          {loading ? (
+            <MutatingDots
+              height="100"
+              width="100"
+              color="#109BA8"
+              secondaryColor="#109BA8"
+              radius="12.5"
+              ariaLabel="mutating-dots-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
             />
-          ))}
+          ) : (
+            savedTracks.map((track) => (
+              <TrackSearchResult
+                track={track}
+                key={track.uri}
+                chooseTrack={chooseTrack}
+              />
+            ))
+          )}
         </div>
-        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+        <Player
+          accessToken={spotifyApi.getAccessToken()}
+          trackUri={playingTrack?.uri}
+        />
       </Container>
     </>
   );
 };
 
 export default Library;
+
+// spotifyApi.removeFromMySavedTracks(["3VNWq8rTnQG6fM1eldSpZ0"])
+// .then(function(data) {
+//   console.log('Removed!');
+// }, function(err) {
+//   console.log('Something went wrong!', err);
+// });
