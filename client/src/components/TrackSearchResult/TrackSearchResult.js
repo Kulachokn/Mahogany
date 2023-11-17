dd07803cae12f865234733a2b7a79cd4532589c2
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2023-11-17 17:29:58 +0100

    added icon to add-to-favorietes button + hover effects to items

--- client/src/components/TrackSearchResult/TrackSearchResult.js
+++ client/src/components/TrackSearchResult/TrackSearchResult.js
@@ -78,7 +78,56 @@ const TrackSearchResult = ({
             </svg>
           </button>
         ) : (
-          <button type="button">Add to favorites</button>
+          <button type="button" className={styles.btn}>
+            <svg
+            className={styles.addIcon}
+              fill="#109BA8"
+              viewBox="0 0 24 24"
+              id="add-playlist"
+              data-name="Flat Line"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                id="secondary"
+                cx="6.5"
+                cy="17.5"
+                r="3.5"
+                fill="#109BA8"
+                strokeWidth="2"
+              ></circle>
+              <path
+                id="primary"
+                d="M14,15h7M17,5h4M19,3V7M14,19h7"
+                fill="none"
+                stroke="#109BA8"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+              ></path>
+              <circle
+                id="primary-2"
+                data-name="primary"
+                cx="6.5"
+                cy="17.5"
+                r="3.5"
+                fill="none"
+                stroke="#109BA8"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+              ></circle>
+              <path
+                id="primary-3"
+                data-name="primary"
+                d="M14,10a2.86,2.86,0,0,0-1-4,23.19,23.19,0,0,1-3-3V17.5"
+                fill="none"
+                stroke="#109BA8"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+              ></path>
+            </svg>
+          </button>
         )}
         <button type="button" onClick={handleOpenMenu} className={styles.btn}>
           <svg
