63e78935552d4f2e3a93f12c6b07f6c85fe40971
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2023-11-22 17:54:48 +0100

    added functional for input appearance + icon for addPlaylist button

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Form } from "react-bootstrap";
 import SpotifyWebApi from "spotify-web-api-node";
-import Button from "react-bootstrap/Button";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
@@ -12,6 +11,7 @@ import CreatePlaylistModal from "../components/CreatePlaylistModal/CreatePlaylis
 import { getSmallestAlbumImage } from "../utils/getSmallestAlbumImage";
 import { convertTrackDuration } from "../utils/convertTrackDuration";
 import { getUserProfile } from "../utils/getUserProfile";
+import styles from "./Dashboard.module.css";
 
 import PlaylistsGallery from "../components/PlaylistsGallery";
 
@@ -26,7 +26,8 @@ const Dashboard = ({ code }) => {
   const [searchResults, setSearchResults] = useState([]);
   const [playingTrack, setPlayingTrack] = useState();
   const [show, setShow] = useState(false);
-  const [updateSavedPlaylists, setUpdateSavedPlaylists] = useState(false)
+  const [updateSavedPlaylists, setUpdateSavedPlaylists] = useState(false);
+  const [formVisible, setFormVisible] = useState(false);
 
   useEffect(() => {
     if (!accessToken || typeof accessToken !== "string") return;
@@ -52,8 +53,8 @@ const Dashboard = ({ code }) => {
         const tracks = response.body.tracks.items.map((item) => {
           const smallestAlbumImage = getSmallestAlbumImage(item.album);
           const getArtists = item.artists
-          .map((artist) => artist.name)
-          .join(", ");
+            .map((artist) => artist.name)
+            .join(", ");
 
           return {
             artist: getArtists,
@@ -92,7 +93,7 @@ const Dashboard = ({ code }) => {
           position: "bottom-right",
           hideProgressBar: true,
         });
-        setUpdateSavedPlaylists(true)
+        setUpdateSavedPlaylists(true);
       })
       .catch((err) => {
         console.log(err.message);
@@ -119,23 +120,65 @@ const Dashboard = ({ code }) => {
     }
   };
 
+  const handleButtonClick = () => {
+    setFormVisible(!formVisible);
+  };
+
+  const handleInputBlur = () => {
+    if (search.trim() === "") {
+      setFormVisible(false);
+    }
+  };
+
   return (
     <Container className="d-flex flex-column" style={{ height: "100vh" }}>
-      <ToastContainer />
-      <Button variant="primary" onClick={handleShow}>
-        &#43;
-      </Button>
-      <CreatePlaylistModal
-        onHide={handleCloseModal}
-        show={show}
-        onFormSubmit={handleFormSubmit}
-      />
-      <Form.Control
-        type="search"
-        placeholder="Search Song"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+      <div className={styles.controllersWrap}>
+        <button
+          type="button"
+          onClick={handleShow}
+          className={styles.addPlaylist}
+        >
+          <svg
+            className={styles.addPlaylistIcon}
+            viewBox="0 0 32 32"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M15.5 29.5c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13zM21.938 15.938c0-0.552-0.448-1-1-1h-4v-4c0-0.552-0.447-1-1-1h-1c-0.553 0-1 0.448-1 1v4h-4c-0.553 0-1 0.448-1 1v1c0 0.553 0.447 1 1 1h4v4c0 0.553 0.447 1 1 1h1c0.553 0 1-0.447 1-1v-4h4c0.552 0 1-0.447 1-1v-1z"></path>
+          </svg>
+        </button>
+        <div className={styles.searchWrap}>
+          <button
+            type="button"
+            className={styles.searchBtn}
+            onClick={handleButtonClick}
+          >
+            <svg
+              className={styles.searchIcon}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
+        {formVisible && (
+          <Form.Control
+            type="search"
+            placeholder="Search Song"
+            className={styles.form}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onBlur={handleInputBlur}
+          />
+        )}
+      </div>
       <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
         {searchResults.length > 1 ? (
           searchResults.map((track, ind) => (
@@ -149,12 +192,18 @@ const Dashboard = ({ code }) => {
             />
           ))
         ) : (
-          <PlaylistsGallery updateSavedPlaylists={updateSavedPlaylists}/>
+          <PlaylistsGallery updateSavedPlaylists={updateSavedPlaylists} />
         )}
       </div>
       <Player
         accessToken={accessToken ? accessToken : storedAccessToken}
         trackUri={playingTrack?.uri}
+      />
+      <ToastContainer />
+      <CreatePlaylistModal
+        onHide={handleCloseModal}
+        show={show}
+        onFormSubmit={handleFormSubmit}
       />
     </Container>
   );
